0eac6e92e21e7a1913abf934b18419abb5506bf4
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-10-21 19:31:06 -0700

    Changed to better reflect the Law of Demeter

--- main.rb
+++ main.rb
@@ -5,9 +5,7 @@ class Student
     course_list.remove(course)
   end
 
-  def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
+  def add_to_schedule(course, course_list)
     max_courses = course_list.maximum_number_of_courses
     if max_courses < MAX_COURSES
         course_list.add(course)
